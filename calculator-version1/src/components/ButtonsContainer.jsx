16cd93a1552f9e4e3ac4ae7b87efba6796eceba6
Sojwal <sojwalkuthe@gmail.com>
2024-07-12 19:58:04 +0530

    imrovement in programm

--- calculator-version1/src/components/ButtonsContainer.jsx
+++ calculator-version1/src/components/ButtonsContainer.jsx
@@ -2,7 +2,7 @@ import styles from "./ButtonsContainer.module.css";
 
 
 const ButtonsContainer = () => {
-const buttonNames=['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.'];
+const buttonNames=['C','1','2','3','+','4','-','5','6','*','7','8','/','=','9','0','.'];
   return (
     <div className={styles.buttonsContainer}>
      
